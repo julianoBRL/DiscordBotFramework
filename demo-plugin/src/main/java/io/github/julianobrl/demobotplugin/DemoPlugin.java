beb52d689cf35135bdfc48b754c143b9b35d802a
JulianoBRL <juliano0forum@gmail.com>
2025-09-06 14:05:01 -0300

    Added Parent

--- demo-plugin/src/main/java/io/github/julianobrl/demobotplugin/DemoPlugin.java
+++ demo-plugin/src/main/java/io/github/julianobrl/demobotplugin/DemoPlugin.java
@@ -1,27 +1,29 @@
 package io.github.julianobrl.demobotplugin;
 
 import io.github.julianobrl.demobotplugin.commands.slash.TestCommand;
-import io.github.julianobrl.discordbots.framework.annotations.BotPlugin;
 import io.github.julianobrl.discordbots.framework.managers.PrefixCommandsManager;
 import io.github.julianobrl.discordbots.framework.managers.SlashCommandsManager;
-import io.github.julianobrl.discordbots.framework.plugins.AbstractBotPlugin;
+import lombok.extern.slf4j.Slf4j;
+import org.pf4j.Plugin;
 
-@BotPlugin(
-    id = "demo",
-    name = "demo",
-    author = "Shintarobrl",
-    version = "1.0.1",
-    description = "Just a demo plugin"
-)
-public class DemoPlugin extends AbstractBotPlugin {
+@Slf4j
+public class DemoPlugin extends Plugin {
 
     @Override
-    public void onEnable(){
-        System.out.println("demo onEnable-1");
+    public void start() {
+        log.info("DemoPlugin-Start");
         SlashCommandsManager.getInstance().addCommand(TestCommand.class);
-        System.out.println("demo onEnable-2");
         PrefixCommandsManager.getInstance().addCommand(io.github.julianobrl.demobotplugin.commands.prefix.TestCommand.class);
-        System.out.println("demo onEnable-3");
+    }
+
+    @Override
+    public void stop() {
+        log.info("DemoPlugin-Stop");
+    }
+
+    @Override
+    public void delete() {
+        log.info("DemoPlugin-Delete");
     }
 
 }
